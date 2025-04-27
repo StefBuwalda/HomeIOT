0458a5ac6b4da6bed56658fb23ce42db2a52f945
Stef <stbuwalda@gmail.com>
2025-04-27 10:03:23 +0200

    Example chart on homepage using chart.js

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from application import app
+from flask import render_template
+import random
+
+
+@app.route("/")
+def index():
+    # You could load real data here instead of random
+    temps = [random.randint(18, 25) for _ in range(24)]  # Example: 24 hours
+    return render_template("index.html", temps=temps)
+
 
 if __name__ == "__main__":
     app.run()
